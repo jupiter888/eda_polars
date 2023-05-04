6dfaf107ba8c1ab85d9c4a6d62118b126629dbc8
Daniel <visual.eyes.success@gmail.com>
2023-05-04 13:37:24 +0200

    Print row enums containing null and nan values

--- filtering.py
+++ filtering.py
@@ -1,15 +1,34 @@
 import polars as pl
 
-# Some DataFrame named df
-
 # [-------- FILTERING BY VALUE --------]
 
+# Some DataFrame named df
 # Filter the data to include only rows where column 'value_col' is greater than 100
 filtered_data = df.filter(pl.col('value_col') > 100)
 
 
+
+
+# [-------- PRINT ROW ENUM VALS CONTAINING NULL & NAN VALUES --------]
+# create a DataFrame with missing values
+df = pl.DataFrame({
+    'A': [1, 2, None],
+    'B': [14, None, 888],
+    'C': ['octo', 'pus', None]
+})
+
+# filter rows with missing values
+missing_df = df.filter(pl.col('A').is_null() | pl.col('B').is_null() | pl.col('C').is_null())
+
+# print the enumerator row numbers of the missing values
+print(missing_df.select(pl.row_number()).collect())
+
+
+
+
 # [-------- FILTERING USING CONDITIONS --------]
-# Filtering is not null, Filtering numberic conditions, filtering nulls 
+# FILTERINGG !NULL, FILTERING NUMERIC CONDITIONS, FILTERING NULLS
+
 df = pl.DataFrame({
     'A': [1, 2, 4, None, 8],
     'B': ['clap', 'snare_drum', 'bass_drum', None, 'crash']
@@ -29,14 +48,15 @@ print(filtered)
 
 
 
+
 # [-------FILTERING TEST|TRAIN FROM COLUMN 'DATA' --------]
 # CREATING 2 NEW COLUMNS(TRAIN & TEST) FOR ROWS WITH RESPECTIVE VALUES
 
 # Create sample data
 df = pl.DataFrame({
     'data': ['train', 'train', 'test', 'train', 'test', 'train','test','train'],
-    'sample_name': ['j','u','p','i','t','e','r','s'],
-    'sampe_numeric': [8,88,888,8888,88888,888888,8888888,88888888]
+    'sample_alpha': ['j','u','p','i','t','e','r','s'],
+    'sample_numeric': [8,88,888,8888,88888,888888,8888888,88888888],
 })
 
 # Filter for rows where 'data' column contains 'train'
